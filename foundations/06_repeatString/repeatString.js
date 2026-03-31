84083611397dfd6eeed4a1b351d92c958b68e2bb
brazzo-ba <heromasterhelp@gmail.com>
2026-03-31 14:41:26 +0200

    for loops are confusing

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,6 +1,19 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
 
+let arr = "";
+
+    if(num >= 0) { 
+    for(i=0; i < num; i++){
+        arr += string;
+    }
+}
+    else if (num < 0) {
+     arr = "ERROR";
+    }
+    return arr;
 };
+
+console.log(repeatString('hey', -1))
 
 // Do not edit below this line
 module.exports = repeatString;
