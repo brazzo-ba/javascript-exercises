79f3a2f77271f733f3d13de8ae7138906c880e97
brazzo-ba <heromasterhelp@gmail.com>
2026-04-01 14:22:18 +0200

    really confusing but we move

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...num) {
+    let result = arr.filter((numbers) => !num.includes(numbers));
+    return result;
 };
 
+
+
+console.log(removeFromArray([1, 2, 3, 4, 5, 6, 7, 8], 7))
 // Do not edit below this line
 module.exports = removeFromArray;
