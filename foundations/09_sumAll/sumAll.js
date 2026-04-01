a9ed1942622b9b459ca81a8e9facff780e568210
brazzo-ba <heromasterhelp@gmail.com>
2026-04-01 15:21:59 +0200

    finished

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,6 +1,32 @@
-const sumAll = function() {
+const sumAll = function(numOne, numRange) {
+    let sum = 0;
+    if(
+        Number.isInteger(numOne, numRange) &&
+        numOne < numRange &&
+        (numOne >= 0 && numRange >= 0) &&
+        typeof (numOne) === 'number' &&
+        typeof (numRange) === 'number' &&
+        !isNaN(Number(numOne)) &&
+        !isNaN(Number(numRange))
+    ) {
+        for(let i = numOne; i <= numRange; i++) {
+    sum += i;
+    }
+    return sum;
+    } else if(numRange < numOne) {
+        for(let i = numRange; i <= numOne; i++) {
+    sum += i;
+    }
+    return sum;
+    }
+    
+    else {
+        return 'ERROR';
+    }
 
+
+    
 };
-
+console.log(sumAll(1,4));
 // Do not edit below this line
 module.exports = sumAll;
