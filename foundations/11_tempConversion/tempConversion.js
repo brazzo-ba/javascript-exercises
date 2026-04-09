780b5623f038a4d8e61fed4e90f4366f490e4e48
brazzo-ba <heromasterhelp@gmail.com>
2026-04-09 14:33:23 +0200

    functions well. very easy.

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,9 +1,14 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  const result = (((fahrenheit - 32)*5)/9);
+  return Math.round(result * 10) / 10
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  const result = (celsius * 1.8) + 32;
+  return Math.round(result * 10) / 10
 };
-
+console.log(convertToCelsius(32));
+console.log(convertToFahrenheit(0));
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
